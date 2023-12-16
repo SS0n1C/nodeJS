9829a8472ada0e6a9f2038400160f1fe83839455
ss0n1c <lls.s0n1cll@gamil.com>
2023-12-16 19:28:03 +0200

    add convector modul

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-let num = 5
-console.log("variable:" + num)
+let con = require("currency-converter-lt")
+let Convector = new con({from:"USD", to:"UAH", amount:100})
+Convector.convert().then((res) => {
+    console.log(`Resuly: ${res}`)
+})
